e7f971b2310fe49179516fa115c6ccfc3c7b5b32
cristianquirama0506 <cristia.quirama@est.iudigital.edu.co>
2023-11-26 20:33:25 -0500

    primero

---
+++ js/registrocaso.js
@@ -0,0 +1,7 @@
+document.getElementById('registroCasoForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Lógica para manejar el envío del formulario de registro de caso
+    alert('¡Caso registrado correctamente!');
+  });
+  
---
+++ js/registrousuario.js
@@ -0,0 +1,7 @@
+document.getElementById('registroUsuarioForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Lógica para manejar el envío del formulario de registro de usuario
+    alert('¡Usuario registrado correctamente!');
+  });
+  
